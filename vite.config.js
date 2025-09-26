1f433fb69b09a7e9b9f9003cdc83dd93c0fdeb60
Chaitanya <chaitanya20022003@gmail.com>
2025-09-26 14:30:33 -0500

    Make base dynamic for Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+const isVercel = process.env.VERCEL === "1";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio/",
+  base: isVercel ? "/" : "/my-portfolio/",
 })
